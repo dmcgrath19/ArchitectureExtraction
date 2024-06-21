a8ad7781f0f3da3ca713b37a6cc58452bb17988a
dmcgrath19 <s2558433@ed.ac.uk>
2024-06-21 11:20:59 +0100

    move rogue files from the scratch to this git

--- pythia_scripts/pythia_script.sh
+++ pythia_scripts/pythia_script.sh
@@ -12,7 +12,12 @@ JOB_NAME="pythia-160m"
 #$ -l h_vmem=4G
 #$ -l h_rt=48:00:00
 
-# Create / activate conda env if it doesn't exist
+export HF_HOME="/exports/eddie/scratch/s2558433/huggingface_cache"
+export TRANSFORMERS_CACHE="/exports/eddie/scratch/s2558433/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="/exports/eddie/scratch/s2558433/huggingface_cache/datasets"
+export PIP_CACHE_DIR="/exports/eddie/scratch/s2558433/.cache/pip"
+export CONDA_PKGS_DIRS="/exports/eddie/scratch/s2558433/.cache/conda_pkgs"
+export HF_DATASETS_CACHE="/exports/eddie/scratch/s2558433/.cache/huggingface_datasets"
 
 source /exports/eddie/scratch/s2558433/miniconda3/etc/profile.d/conda.sh
 
