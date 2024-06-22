bec372bec54f0fbb4c2ba92311fef3af2bfce6f7
dmcgrath19 <s2558433@ed.ac.uk>
2024-06-22 13:43:53 +0100

    remove detach

--- main.py
+++ main.py
@@ -91,10 +91,10 @@ def main(args):
 
     try:
         if scores["XL"].device.type == 'cuda':
-            scores["XL"] = scores["XL"].detach().cpu().numpy()
-            scores["S"] = scores["S"].detach().cpu().numpy()
-            scores["Lower"] = scores["Lower"].detach().cpu().numpy()
-            scores["zlib"] = scores["zlib"].detach().cpu().numpy()
+            scores["XL"] = scores["XL"].cpu().numpy()
+            scores["S"] = scores["S"].cpu().numpy()
+            scores["Lower"] = scores["Lower"].cpu().numpy()
+            scores["zlib"] = scores["zlib"].cpu().numpy()
         elif scores["XL"].device.type == 'cpu':
             scores["XL"] = np.asarray(scores["XL"])
             scores["S"] = np.asarray(scores["S"])
