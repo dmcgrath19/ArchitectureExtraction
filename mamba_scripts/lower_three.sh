a34bf0125ebfc547bf01f2927ce0fc3e29774294
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-05 01:40:13 +0100

    load conda in scripts

--- mamba_scripts/lower_three.sh
+++ mamba_scripts/lower_three.sh
@@ -26,7 +26,10 @@ module unload cuda
 
 module load cuda/12.1.1
 
+source /exports/eddie/scratch/s2558433/miniconda3/etc/profile.d/conda.sh
+module load anaconda
 
+cd /exports/eddie/scratch/s2558433/ArchitectureExtraction/
 conda activate mambafour
 
 python main.py --N 10000 --batch-size 10 --model1 state-spaces/mamba-2.8b-hf --model2 state-spaces/mamba-130m-hf --corpus-path "monology/pile-uncopyrighted" --name-tag "10kpile" --is-mamba
--- mamba_scripts/subset-mamba.sh
+++ mamba_scripts/subset-mamba.sh
@@ -24,11 +24,14 @@ export TOKENIZERS_PARALLELISM=false
 
 . /etc/profile.d/modules.sh
 module unload cuda
-
 module load cuda/12.1.1
 
-conda activate mambafour
+source /exports/eddie/scratch/s2558433/miniconda3/etc/profile.d/conda.sh
+module load anaconda
 
+
+cd /exports/eddie/scratch/s2558433/ArchitectureExtraction/
+conda activate mambafour
 
 python main.py --N 100 --batch-size 10 --model1 state-spaces/mamba-2.8b-hf --model2 state-spaces/mamba-370m-hf --corpus-path 'KaiNylund/WMT-year-splits' --split "2021_train" --name-tag 10k-base --is-mamba --is-wmt
 
