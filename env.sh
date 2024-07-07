b65a9ad9f51e868dc180aee1ffd6ac860c6cb9d2
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-07 13:33:55 +0100

    fix the script

--- env.sh
+++ env.sh
@@ -7,30 +7,28 @@
 #SBATCH --time=0-08:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
-
 export CUDNN_HOME=/opt/cuDNN-7.0/
-
 export STUDENT_ID=$(whoami)
-
 export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
-
 export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
-
 export CPATH=${CUDNN_HOME}/include:$CPATH
-
 export PATH=${CUDA_HOME}/bin:${PATH}
-
 export PYTHON_PATH=$PATH
 
-mkdir -p /disk/scratch/${STUDENT_ID}
+
+export HF_HOME="home/s2558433/.cache/huggingface_cache"
+export TRANSFORMERS_CACHE="home/s2558433/.cache/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="home/s2558433/.cache/huggingface_cache/datasets"
+export PIP_CACHE_DIR="home/s2558433/.cache/pip"
+export CONDA_PKGS_DIRS="home/s2558433/.cache/conda_pkgs"
+
+export CXXFLAGS="-std=c99"
+export CFLAGS="-std=c99"
+export TOKENIZERS_PARALLELISM=false
 
 
-export TMPDIR=/disk/scratch/${STUDENT_ID}/
-export TMP=/disk/scratch/${STUDENT_ID}/
+source /home/${STUDENT_ID}/miniconda3/bin/activate mambafour
+cd /home/s2558433/ArchitectureExtraction/local-env/
 
-mkdir -p ${TMP}/datasets/
-export DATASET_DIR=${TMP}/datasets/
-# Activate the relevant virtual environment:
+pip install -r mamba-requirements.txt
 
-source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
-cd ..
