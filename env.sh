a38efa248fdd9b3ee160579339c13e3ca17e9026
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-07 14:15:36 +0100

    fix naming

--- env.sh
+++ env.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
-#SBATCH -e job_errors.log  # error file for stderr
-#SBATCH -o job_output.log  # output file for stdout
+#SBATCH -e home/s2558433/job_errors_$JOB_ID.log  # error file for stderr
+#SBATCH -o home/s2558433/job_output_$JOB_ID.log  # output file for stdout
 
 # export CUDA_HOME=/opt/cuda-9.0.176.1/
 # export CUDNN_HOME=/opt/cuDNN-7.0/
