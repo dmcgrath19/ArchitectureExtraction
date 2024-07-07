dac886ecc931333a351e8ea1d0c68f2a82548898
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-07 13:50:01 +0100

    try diff cuda config

--- env.sh
+++ env.sh
@@ -6,15 +6,19 @@
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
-export CUDA_HOME=/opt/cuda-9.0.176.1/
-export CUDNN_HOME=/opt/cuDNN-7.0/
+# export CUDA_HOME=/opt/cuda-9.0.176.1/
+# export CUDNN_HOME=/opt/cuDNN-7.0/
 export STUDENT_ID=$(whoami)
-export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
-export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
-export CPATH=${CUDNN_HOME}/include:$CPATH
-export PATH=${CUDA_HOME}/bin:${PATH}
-export PYTHON_PATH=$PATH
+# export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+# export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+# export CPATH=${CUDNN_HOME}/include:$CPATH
+# export PATH=${CUDA_HOME}/bin:${PATH}
+# export PYTHON_PATH=$PATH
 
+
+. /etc/profile.d/modules.sh
+module unload cuda
+module load cuda/12.1.1
 
 export HF_HOME="home/s2558433/.cache/huggingface_cache"
 export TRANSFORMERS_CACHE="home/s2558433/.cache/huggingface_cache/transformers"
