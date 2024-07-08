c0d4181702e9b037868196cd4053278040581966
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-08 09:16:11 +0100

    inc mem

--- pythia160-lg.sh
+++ pythia160-lg.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #SBATCH --partition=PGR-Standard
+#SBATCH --mem=12000
 #SBATCH --gres=gpu:1
 
 python mlp-main.py --N 10000 --batch-size 10 --model1 models/pythia-1.4b --model2 models/pythia-160m --corpus-path "pile.txt" --is-local --name-tag "900step" --input-len 900
