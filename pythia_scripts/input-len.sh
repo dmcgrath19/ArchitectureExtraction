f294dd4e64c76ce2e55de8575e58533d3e372f16
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-08 22:13:09 +0100

    fix small model size for mamba

---
+++ pythia_scripts/input-len.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Use the variable for the job name and log/error files
+#$ -N inputLlen-mamba
+#$ -o /exports/eddie/scratch/s2558433/job_runs/up-mam-$JOB_ID.log
+#$ -e /exports/eddie/scratch/s2558433/job_runs/up-mam-$JOB_ID.err
+#$ -cwd
+#$ -q gpu
+#$ -pe gpu-a100 1
+#$ -l h_vmem=300G
+#$ -l h_rt=24:00:00
+#$ -m bea -M s2558433@ed.ac.uk 
+export HF_HOME="/exports/eddie/scratch/s2558433/.cache/huggingface_cache"
+export TRANSFORMERS_CACHE="/exports/eddie/scratch/s2558433/.cache/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="/exports/eddie/scratch/s2558433/.cache/huggingface_cache/datasets"
+export PIP_CACHE_DIR="/exports/eddie/scratch/s2558433/.cache/pip"
+export CONDA_PKGS_DIRS="/exports/eddie/scratch/s2558433/.cache/conda_pkgs"
+
+export CXXFLAGS="-std=c99"
+export CFLAGS="-std=c99"
+export TOKENIZERS_PARALLELISM=false
+
+. /etc/profile.d/modules.sh
+module unload cuda
+module load cuda/12.1.1
+
+#qlogin -q gpu -pe gpu-a100 1 -l h_vmem=500G -l h_rt=24:00:00
+
+source /exports/eddie/scratch/s2558433/miniconda3/etc/profile.d/conda.sh
+module load anaconda
+
+cd /exports/eddie/scratch/s2558433/ArchitectureExtraction/
+
+conda activate pythia
+
+
+
+python main.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-2.8b --corpus-path "monology/pile-uncopyrighted" --name-tag "input-50"  --input-len 50  
+
+python main.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-1b --corpus-path "monology/pile-uncopyrighted" --name-tag "input-450"  --input-len 450
+
+python main.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-160m --corpus-path "monology/pile-uncopyrighted" --name-tag "input-450"  --input-len 450
+
+
+
+
+
+conda deactivate 
