74b0ea5f0792743ee0903da7a47da3f2d274408e
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-23 20:06:51 +0100

    update rerun for pythia

--- pythia_scripts/subset-pythia.sh
+++ pythia_scripts/subset-pythia.sh
@@ -21,7 +21,13 @@ source /exports/eddie/scratch/s2558433/miniconda3/etc/profile.d/conda.sh
 
 cd /exports/eddie/scratch/s2558433/ArchitectureExtraction/
 
+conda remove --name pythia --all
+
+conda create -n pythia python=3.9 
+
 conda activate pythia
+
+pip install -r requirements.txt
 
 # python main.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-160m --corpus-path "ArmelR/the-pile-splitted" --corpus-subset "FreeLaw" --name-tag "10kfree-input-450" --is-splitted --input-len 450
 
